f02ce9b90bb26f5364c82f3a2b89c91440a134c1
Satenik Oganisyan <satenik_oganisyan@mail.ru>
2023-05-08 21:16:28 +0400

    Add js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// let bool = true;
+
+// document.querySelector(".menu").addEventListener("click", () => {
+//   if (bool) {
+//     document.getElementById("menu_list").style.height = "calc(100vh - 50px)";
+//     document.querySelectorAll("#menu_list > h5").forEach((item) => {
+//       item.style.visibility = "visible";
+//     });
+//     console.log(document.querySelectorAll("#menu_list > h5"));
+//     bool = false;
+//   } else {
+//     document.getElementById("menu_list").style.height = "0";
+//     document.querySelectorAll("#menu_list > h5").forEach((item) => {
+//       item.style.visibility = "hidden";
+//     });
+//     bool = true;
+//   }
+// });
